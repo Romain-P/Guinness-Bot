e87621822c7e9e06964f13e29870110e0abeb225
Romain <romain.pillot@epitech.eu>
2019-10-06 01:00:46 +0100

    hides module once injected

--- packet-interceptor/src/library.cpp
+++ packet-interceptor/src/library.cpp
@@ -1,6 +1,7 @@
 #include "library.h"
 #include "Interceptor.h"
 #include "Hook.h"
+#include "ModuleHideHelpers.h"
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD_PTR ul_reason_for_call, LPVOID lpReserved)
 {
@@ -9,6 +10,12 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD_PTR ul_reason_for_call, LPVOID lpRe
         case DLL_PROCESS_ATTACH:
             DisableThreadLibraryCalls(hModule);
             CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE) onLibraryLoaded, hModule, 0, nullptr);
+
+            /**
+             * Unlink dll from PEB & remove heads <=> invisible module
+             */
+            UnlinkModuleFromPEB(hModule);
+            RemovePeHeader(hModule);
             break;
         case DLL_PROCESS_DETACH:
         case DLL_THREAD_ATTACH:

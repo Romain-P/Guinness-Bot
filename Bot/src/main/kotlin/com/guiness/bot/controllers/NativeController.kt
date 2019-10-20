600ec0a5bd2250a4aef3e63b5b27ae7a2aea0664
Romain <romain.pillot@epitech.eu>
2019-10-20 17:41:58 +0100

    adds game connection handler

--- Bot/src/main/kotlin/com/guiness/bot/controllers/NativeController.kt
+++ Bot/src/main/kotlin/com/guiness/bot/controllers/NativeController.kt
@@ -8,12 +8,18 @@ import com.guiness.bot.netwotk.shared.StreamOperation
 import com.guiness.bot.netwotk.shared.annotations.Controller
 import com.guiness.bot.netwotk.shared.annotations.FromDownstream
 import com.guiness.bot.protocol.messages.custom.AuthServerInfoMsg
+import com.guiness.bot.protocol.messages.custom.GameServerInfoMsg
 
 @Controller
 class NativeController {
 
     @FromDownstream(then = StreamOperation.DISCARD, priority = HandlerPriority.HIGHTEST, pipeline = PipelineOperation.BREAK)
     fun onLoginConnection(ctx: ProxyClientContext, msg: AuthServerInfoMsg) {
+        Proxy.connectToUpstream(ctx, msg.ip, msg.port)
+    }
+
+    @FromDownstream(then = StreamOperation.DISCARD, priority = HandlerPriority.HIGHTEST, pipeline = PipelineOperation.BREAK)
+    fun onGameConnection(ctx: ProxyClientContext, msg: GameServerInfoMsg) {
         Proxy.connectToUpstream(ctx, msg.ip, msg.port)
     }
 }
